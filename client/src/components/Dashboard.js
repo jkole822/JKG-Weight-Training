4fc42e07793fc4c241b70b9076f701c0c0e92b80
Kole Gasior <jkole822@gmail.com>
2020-12-09 15:57:15 -0500

    conditionally render dashboard buttons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 import { Link } from "react-router-dom";
 
-const Dashboard = () => {
-	return (
-		<div className="row">
-			<Link to="/workouts/log" className="btn btn-large teal col s12 m4">
-				Start Workout
-			</Link>
-			<Link to="/workouts/deload" className="btn btn-large indigo col s12 m4">
-				Deload Weight
-			</Link>
-			<Link to="/workouts/new" className="btn btn-large red col s12 m4">
-				Begin New Program
-			</Link>
-		</div>
-	);
-};
+class Dashboard extends Component {
+	componentDidMount() {
+		this.props.fetchStats();
+	}
 
-export default Dashboard;
+	renderLog() {
+		if (this.props.stats) {
+			return (
+				<div>
+					<Link to="/workouts/log" className="btn btn-large teal col s12 m4">
+						Start Workout
+					</Link>
+					<Link
+						to="/workouts/deload"
+						className="btn btn-large indigo col s12 m4"
+					>
+						Deload Weight
+					</Link>
+				</div>
+			);
+		}
+	}
+
+	render() {
+		return (
+			<div className="row">
+				{this.renderLog()}
+				<Link
+					to="/workouts/new"
+					className={
+						this.props.stats
+							? "btn btn-large red col s12 m4"
+							: "btn btn-large red col s12 m4 offset-m4"
+					}
+				>
+					Begin New Program
+				</Link>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps({ stats }) {
+	return { stats };
+}
+
+export default connect(mapStateToProps, actions)(Dashboard);

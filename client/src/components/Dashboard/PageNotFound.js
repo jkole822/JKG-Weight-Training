8a1364a7f143a43a8fbe8e89b63294c94a0718de
Kole Gasior <jkole822@gmail.com>
2021-01-04 21:47:21 -0500

    remove buttons from 404 page

--- client/src/components/Dashboard/PageNotFound.js
+++ client/src/components/Dashboard/PageNotFound.js
@@ -1,30 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
-import MediaQuery from "react-responsive";
-import { connect } from "react-redux";
-
-import DesktopButtons from "./DesktopButtons";
-import MobileButtons from "./MobileButtons";
 
 const PageNotFound = ({ stats }) => {
+	console.log(stats);
 	return (
-		<div>
-			<div id="not-found" className="light-blue-text text-darken-3">
-				404: Page not found.
-			</div>
-			<MediaQuery minWidth={993}>
-				<DesktopButtons stats={stats} />
-			</MediaQuery>
-			{/* Render click-to-toggle FABs on small screens */}
-			<MediaQuery maxWidth={992}>
-				<MobileButtons stats={stats} />
-			</MediaQuery>
+		<div id="not-found" className="light-blue-text text-darken-3">
+			404: Page not found.
 		</div>
 	);
 };
 
-function mapStateToProps({ stats }) {
-	return { stats };
-}
-
-export default connect(mapStateToProps)(PageNotFound);
+export default PageNotFound;
--- client/src/components/__tests__/App.test.js
+++ client/src/components/__tests__/App.test.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { mount } from "enzyme";
+
 import Root from "../../Root";
 import App from "../App";
 import Header from "../Header";
 import Footer from "../Footer";
 
-let wrapper;
+describe("Header and Footer are rendered", () => {
+	let wrapper;
 
-beforeEach(() => {
-	wrapper = mount(
-		<Root>
-			<App />
-		</Root>
-	);
-});
+	beforeEach(() => {
+		wrapper = mount(
+			<Root>
+				<App />
+			</Root>
+		);
+	});
 
-afterEach(() => {
-	wrapper.unmount();
-});
+	afterEach(() => {
+		wrapper.unmount();
+	});
+	it("shows the Header component", () => {
+		expect(wrapper.find(Header).length).toEqual(1);
+	});
 
-it("shows the Header component", () => {
-	expect(wrapper.find(Header).length).toEqual(1);
-});
-
-it("shows the Footer component", () => {
-	expect(wrapper.find(Footer).length).toBe(1);
+	it("shows the Footer component", () => {
+		expect(wrapper.find(Footer).length).toBe(1);
+	});
 });
